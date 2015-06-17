57370295a4527c83b91ba0560ba1d1beec210db1
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-06-16 21:11:38 -0500

    Scope facebook

--- src/app/lib/hefesoft.standard/fb/directivas/signUp/directive/directive.js
+++ src/app/lib/hefesoft.standard/fb/directivas/signUp/directive/directive.js
@@ -88,7 +88,7 @@ angular.module('hefesoft.google')
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
+    FB.api('/me', {fields: "id,name,picture,email"}, function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
 
